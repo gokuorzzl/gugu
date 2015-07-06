1cd01229c26b3bc6bd646c0fa929b730a1abaad2
Kim Myung Sang <gokuorzzl@naver.com>
2015-07-06 16:20:26 +0900

    y must be int and between 1 and 9

--- gugu.py
+++ gugu.py
@@ -13,7 +13,11 @@ if 2 <= dan <= 9:
     except ValueError:
         print("오류입니다", file=sys.stderr)
         raise SystemExit()
-    print(dan, '*', y, '=', dan*y)
+    if 1 <= y <= 9:
+        print(dan, '*', y, '=', dan*y)
+    else:
+        print("오류입니다", file=sys.stderr)
+        raise SystemExit()
 else:
     for x in range(2, 10):
         for y in range(1, 10):
